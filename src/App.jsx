74c7cb632c94a382dda0eef2ca64cd7f73c301fe
Justine1193 <rodriguezjustined@gmail.com>
2026-05-03 00:57:16 +0800

    Fix router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import DesignGraphics from './pages/DesignGraphics.jsx'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/designs" element={<DesignGraphics />} />
